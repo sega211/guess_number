b99c8c7ba857b173a64f98adf356e822c8379b0e
Sergey Kuznetsov <sega21@mail.ru>
2025-02-12 15:17:54 +0300

    add function Guess

--- main.js
+++ main.js
@@ -9,3 +9,11 @@ console.log(secretNumber);
 console.log(range.max);
 const comp1Name = "Компьютер 1"; 
 const comp2Name = "Компьютер 2"; 
+
+function guess () {
+    guessNumber = Math.trunc(range.max /2);
+    console.log(`${comp2Name} : Пробую число ${guessNumber}`);
+    return guessNumber;
+}
+guess();
+
