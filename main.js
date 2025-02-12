39d91d912dd0ce08621bac6b22555cdbff7ceb02
Sergey Kuznetsov <sega21@mail.ru>
2025-02-12 15:44:48 +0300

    add function Answer

--- main.js
+++ main.js
@@ -11,9 +11,33 @@ const comp1Name = "Компьютер 1";
 const comp2Name = "Компьютер 2"; 
 
 function guess () {
-    guessNumber = Math.trunc(range.max /2);
+    guessNumber = Math.trunc((range.max - range.min + 1) /2);
     console.log(`${comp2Name} : Пробую число ${guessNumber}`);
     return guessNumber;
 }
 guess();
 
+function answer(guessNumber) {
+    let win = 0;
+    if (guessNumber < secretNumber) {
+        range.min = guessNumber + 1;
+        console.log(`${comp1Name}: "Больше"`);
+        return range.min;
+    } else if (guessNumber > secretNumber) {
+        range.max = guessNumber - 1;
+        console.log(`${comp1Name}: "Меньше"`);
+        return range.max;
+    } else {
+        win = 1;
+        console.log(`${comp1Name}: "Угадал"`);
+        return win;
+    }
+}
+
+
+
+// answer(guessNumber);
+// console.log(range);
+// guess();
+// answer(guessNumber);
+// console.log(range);
