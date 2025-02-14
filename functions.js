bb7fc584ace9eda7a8b79008ec55eb1bdaecd386
Sergey Kuznetsov <sega21@mail.ru>
2025-02-14 16:12:59 +0300

    create function mySlice

--- functions.js
+++ functions.js
@@ -1,2 +1,41 @@
 "use strict";
 
+function mySlice(arr, start, end) {
+    if (!Array.isArray(arr)) {
+        console.error("first argument must be an array");
+    }
+
+    if (start === undefined) {
+        start = 0;
+    } else if (start < 0) {
+        
+        start = arr.length + start;
+        if (start < 0) start = 0;
+    }
+
+    
+    if (end === undefined) {
+        end = arr.length;
+    } else if (end < 0) {
+        end = arr.length + end;
+        if (end < 0) end = 0;
+    }
+
+    if (start > end) {
+        return [];
+    }
+
+    const result = [];
+    for (let i = start; i < end; i++) {
+        if (i >= arr.length) break; 
+        result.push(arr[i]);
+    }
+    return result;
+}
+
+const arr1 = [1, 3, 5, 3, "let"];
+
+const newArr = mySlice(arr1, 3, 5 );
+
+console.log(arr1);
+console.log(newArr);
