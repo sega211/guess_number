d76c892159c4896cd539d9ad21af6ce98875fec3
Sergey Kuznetsov <sega21@mail.ru>
2025-02-16 13:31:14 +0300

    add function sortedItems

--- object.js
+++ object.js
@@ -13,3 +13,11 @@ const sumOfNumbers = function(obj) {
 
 // console.log(sumOfNumbers({"name" : "Igor", "age" : 25, "summery" : 12000}));//12025
 // console.log(sumOfNumbers({"car" : "BMV", "age" : 2, "maxSpeed" : 220, "price": 1900000}));//1900222
+
+const sortedItems = function(obj) {
+    const sortedKeys = Object.keys(obj).sort();
+    return sortedKeys;
+
+}
+// console.log(sortedItems({"name" : "Igor", "age" : 25, "summery" : 12000}));// ['age', 'name', 'summery']
+// console.log(sortedItems({"car" : "BMV", "age" : 2, "maxSpeed" : 220, "price": 1900000})); //['age', 'car', 'maxSpeed', 'price']
