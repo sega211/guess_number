042a4d3485ebb9797de5857023f707c6eabe7dff
Sergey Kuznetsov <sega21@mail.ru>
2025-02-16 13:49:03 +0300

    add function upperText

---
+++ string.js
@@ -0,0 +1,6 @@
+const upperText = function(str) {
+    return (str[0].toUpperCase() + str.slice(1));
+}
+// console.log(upperText("lorem-ipsun")); // Lorem-ipsun
+// console.log(upperText("миру мир")); // Миру мир
+// console.log(upperText("Привед медвед")); // Привед медвед
